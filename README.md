f689f6669981de4038215bd62ebc37cc403757c9
Xen0Xys <red57101@gmail.com>
2022-09-28 12:21:10 +0200

    :tada: First commit for MultiMC Plugin Sample (need some tests for maven)

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # MultiMC-Plugin-Sample
+
+## Exporting JAR
+To export the API JAR, please use `maven install` (by default, exported to "server/plugins/")
---
+++ src/main/java/fr/multimc/sample/Plugin.java
@@ -0,0 +1,21 @@
+package fr.multimc.sample;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+@SuppressWarnings("unused")
+public class Plugin extends JavaPlugin {
+    @Override
+    public void onLoad() {
+        super.onLoad();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+}
